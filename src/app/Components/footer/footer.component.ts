64cddb388ed1b84fa821ce9416afe18f1105dd37
GiuseppeMota <giuseppemota@gmail.com>
2024-08-01 17:29:41 -0300

    chore: Add initial project files and structure

---
+++ src/app/Components/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
