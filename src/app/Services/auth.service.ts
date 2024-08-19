de0db45121ad179ec6a0c885ab32ea11b3951ac0
BrendomGoncalves <brendom.vi@gmail.com>
2024-08-19 10:39:20 -0300

    fix: Configurando token

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,100 +1,52 @@
 import { Injectable } from '@angular/core';
 import { User } from '../Models/user';
 import { BehaviorSubject, tap } from 'rxjs';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Router} from "@angular/router";
 
-// interface AuthResponseDataToken {
-//   email: string;
-//   localId: string;
-//   idToken: string;
-//   expiresIn: string;
-// }
+interface AuthResponseData{
+  token: string;
+  expiration: Date;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  url_signup = 'http://localhost:5013/v1/Auth';
+  // url_signup = 'http://localhost:5013/v1/Auth';
   url_login = 'http://localhost:5013/v1/Auth/Login';
   usuario = new BehaviorSubject<User | null>(null);
 
   constructor(private http: HttpClient, private router: Router) {}
 
   // signupUser(email: string, password: string) {
+  //   const headers = new HttpHeaders({
+  //     'Authorization': 'Bearer ' + localStorage.getItem('token')
+  //   });
+  //
   //   return this.http
-  //     .post<AuthResponseData>(this.url_signup, {
-  //       email: email,
-  //       password: password,
-  //     })
+  //     .post<{ success: boolean }>(
+  //       this.url_signup,
+  //       {
+  //         email: email,
+  //         password: password
+  //       },
+  //       { headers }
+  //     )
   //     .pipe(
   //       tap((resData) => {
-  //         const expiracaoData = new Date(
-  //           new Date().getTime() + +resData.expiresIn * 1000
-  //         );
-  //         const usuario = new User(
-  //           resData.email,
-  //           resData.localId,
-  //           resData.idToken,
-  //           expiracaoData
-  //         );
-  //
-  //         this.usuario.next(usuario);
-  //         localStorage.setItem('userData', JSON.stringify(usuario));
+  //         if (resData.success) {
+  //           this.router.navigate(['/login']);
+  //         } else {
+  //           this.router.navigate(['/login']);
+  //         }
   //       })
   //     );
   // }
-
-  // loginUser(email: string, password: string) {
-  //   return this.http
-  //     .post<AuthResponseDataToken>(this.url_login, {
-  //       email: email,
-  //       password: password,
-  //     }).pipe(
-  //       tap((resData) => {
-  //         const expiracaoData = new Date(
-  //           new Date().getTime() + +resData.expiresIn * 1000
-  //         );
-  //         const usuario = new User(
-  //           resData.email,
-  //           resData.localId,
-  //           resData.idToken,
-  //           expiracaoData
-  //         );
-  //         this.usuario.next(usuario);
-  //         localStorage.setItem('userData', JSON.stringify(usuario));
-  //       })
-  //     );
-  // }
-
-  signupUser(email: string, password: string) {
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer ' + localStorage.getItem('token')
-    });
-
-    return this.http
-      .post<{ success: boolean }>(
-        this.url_signup,
-        {
-          email: email,
-          password: password
-        },
-        { headers }
-      )
-      .pipe(
-        tap((resData) => {
-          if (resData.success) {
-            this.router.navigate(['/login']);
-          } else {
-            this.router.navigate(['/login']);
-          }
-        })
-      );
-  }
 
   loginUser(email: string, password: string) {
     return this.http
-      .post<{ token: string, expiration: Date }>(this.url_login, {
+      .post<AuthResponseData>(this.url_login, {
         email: email,
         password: password,
       }).pipe(
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {provideHttpClient, withFetch} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideClientHydration(),
     provideHttpClient(withFetch()),
+    // TODO: Implementar interceptor
+    // { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
 };
