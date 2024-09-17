59fc0bc3eba342a8f9c4ba6adde9c4df99ce67f5
BrendomGoncalves <brendom.vi@gmail.com>
2024-09-17 10:52:30 -0300

    fix: Corrigindo tela login, removendo codigo depreciado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import {ToastModule} from "primeng/toast";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, ToastModule],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ToastModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Ingenico.2D.Barcode';
+  mostrarHeaderFooter = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.mostrarHeaderFooter = !event.url.includes('/login');
+      }
+    });
+  }
 }
