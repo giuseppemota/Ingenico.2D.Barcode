6fc6b14e52d91d6a8da9c2c798352c0905dcc907
BrendomGoncalves <brendom.vi@gmail.com>
2024-09-24 18:33:20 -0300

    fix: Ajustes acesso login e rotas

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CardModule} from "primeng/card";
 import {FormsModule} from "@angular/forms";
 import {MessageModule} from "primeng/message";
@@ -7,6 +7,7 @@ import {InputTextModule} from "primeng/inputtext";
 import {ButtonDirective} from "primeng/button";
 import {AuthService} from "../../Services/auth.service";
 import {Router} from "@angular/router";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-login',
@@ -22,14 +23,22 @@ import {Router} from "@angular/router";
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   loginData = {
     username: "",
     password: ""
   };
+
   errorMessage = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private messageService: MessageService) {}
+
+  ngOnInit() {
+    if (this.authService.usuarioLogado()) {
+      this.messageService.add({severity: 'info', summary: 'Você já está logado!'});
+      this.router.navigate(['/produtos']).then();
+    }
+  }
 
   onSubmit() {
     this.authService.loginUser(this.loginData.username, this.loginData.password).subscribe({
